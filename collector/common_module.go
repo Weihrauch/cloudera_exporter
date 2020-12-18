a48b6982dcd83a5844a80ba8b05c2c1befb357bb
ntumbaphinees <ntumbaphinees@gmail.com>
2020-12-18 06:10:31 +0100

    removed queries

--- collector/common_module.go
+++ collector/common_module.go
@@ -124,7 +124,7 @@ func make_query(ctx context.Context, uri string, user string, passwd string, pcl
   // var errRes *error
   // Make the API request
   if(pclient != nil){
-    res, err = pclient.Do(req)
+    res, err = pclient.DoPool(req)
   }else{
     res, err = httpClient.Do(req)
   }
